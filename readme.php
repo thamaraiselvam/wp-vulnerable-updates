e0cd33f683edeaa351dc43b807eb1b79b247d258
Thamaraiselvam <thamaraiselvam@live.com>
2017-11-10 19:17:51 +0530

    Delete readme.php

--- readme.php
+++
@@ -1,8 +0,0 @@
-=== Vulnerable Updates ===
-Contributors: thamaraiselvam
-Tags: Vulnerable, vulns, plugins, themes, WP vulns
-Requires at least: 4.0.0
-Tested up to: 4.8.1
-Stable tag: 1.0.0
-
-This plugin to find Vulns
